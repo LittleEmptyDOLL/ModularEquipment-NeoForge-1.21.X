ee55a849ef2ba78c3425d2484cf3cabbcaba1698
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-11 18:19:09 +0300

    Expand MatrixState with energy generation and storage

--- src/main/java/com/github/littleemptydoll/exoequipment/matrix/MatrixState.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/matrix/MatrixState.java
@@ -2,6 +2,10 @@ package com.github.littleemptydoll.exoequipment.matrix;
 
 public record MatrixState(
         int energyConsumption,
+        int energyGeneration,
+        int energyStorageCapacity,
+        int energyStorageInput,
+        int energyStorageOutput,
         int heatGeneration,
         int cooling
 ) {

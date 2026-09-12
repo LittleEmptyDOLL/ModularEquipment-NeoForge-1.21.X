f11d43f85e727b370e13841944e3810dbfa2fe6c
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-12 13:20:37 +0300

    Add exoskeleton keybind

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/client/ModKeyMappings.java
@@ -0,0 +1,44 @@
+package com.github.littleemptydoll.exoequipment.client;
+
+import com.github.littleemptydoll.exoequipment.ExoEquipment;
+import com.github.littleemptydoll.exoequipment.network.OpenExoskeletonPayload;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
+import org.lwjgl.glfw.GLFW;
+
+@EventBusSubscriber(modid = ExoEquipment.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public final class ModKeyMappings {
+
+    public static final KeyMapping OPEN_EXOSKELETON = new KeyMapping(
+            "key.exoequipment.open_exoskeleton",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_G,
+            "key.categories.exoequipment"
+    );
+
+    @SubscribeEvent
+    public static void register(RegisterKeyMappingsEvent event) {
+        event.register(OPEN_EXOSKELETON);
+    }
+
+    private ModKeyMappings() {}
+}
+
+@EventBusSubscriber(modid = ExoEquipment.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.GAME)
+final class ModKeyMappingHandler {
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+        while (ModKeyMappings.OPEN_EXOSKELETON.consumeClick()) {
+            PacketDistributor.sendToServer(new OpenExoskeletonPayload());
+        }
+    }
+
+    private ModKeyMappingHandler() {}
+}

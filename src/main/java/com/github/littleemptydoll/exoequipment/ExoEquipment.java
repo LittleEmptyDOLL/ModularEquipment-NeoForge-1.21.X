8ee730dd65741b21628ce70706283881943ee65e
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-12 13:21:18 +0300

    Register exoskeleton networking

--- src/main/java/com/github/littleemptydoll/exoequipment/ExoEquipment.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/ExoEquipment.java
@@ -1,27 +1,24 @@
 package com.github.littleemptydoll.exoequipment;
 
+import com.github.littleemptydoll.exoequipment.network.ModNetworking;
 import com.github.littleemptydoll.exoequipment.registry.*;
-
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ExoEquipment.MODID)
 public class ExoEquipment {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "exoequipment";
     public static final String LOCALE = "en_us";
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public ExoEquipment(IEventBus eventBus) {
-        // Регистрируем абсолютно всё именно тут, что бы не грузить главный класс
         ModRegistries.register(eventBus);
         eventBus.addListener(ModRegistryKeys::register);
 
         ModItems.register(eventBus);
         ModDataComponents.register(eventBus);
         ModMenus.register(eventBus);
+        ModNetworking.register(eventBus);
 
         ModMatrices.MATRICES.register(eventBus);
         ModModules.MODULES.register(eventBus);

acc698351d1be66898af39b9b7ae518d5900aed4
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-13 23:45:56 +0300

    Show stored battery energy in module tooltip

--- src/main/java/com/github/littleemptydoll/exoequipment/item/ModuleItem.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/item/ModuleItem.java
@@ -3,6 +3,7 @@ package com.github.littleemptydoll.exoequipment.item;
 import com.github.littleemptydoll.exoequipment.client.TooltipHelper;
 import com.github.littleemptydoll.exoequipment.module.ModuleDefinition;
 import com.github.littleemptydoll.exoequipment.registry.EquipmentItem;
+import com.github.littleemptydoll.exoequipment.registry.ModDataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -13,25 +14,16 @@ import java.util.List;
 public class ModuleItem extends EquipmentItem<ModuleDefinition> {
 
     public ModuleItem(
-            DeferredHolder<
-                    ModuleDefinition,
-                    ModuleDefinition
-                    > definition,
+            DeferredHolder<ModuleDefinition, ModuleDefinition> definition,
             Properties properties
     ) {
-        super(
-                definition,
-                properties
-        );
+        super(definition, properties);
     }
 
     public static ModuleItem get(ItemStack stack) {
         if (!(stack.getItem() instanceof ModuleItem moduleItem)) {
-            throw new IllegalArgumentException(
-                    "ItemStack is not an module"
-            );
+            throw new IllegalArgumentException("ItemStack is not an module");
         }
-
         return moduleItem;
     }
 
@@ -45,69 +37,31 @@ public class ModuleItem extends EquipmentItem<ModuleDefinition> {
         ModuleDefinition definition = getDefinition();
 
         appendEquipmentTooltip(tooltip);
-
-        tooltip.add(
-                TooltipHelper.category(
-                        definition.category()
-                )
-        );
-
-        tooltip.add(
-                TooltipHelper.size(
-                        definition.size().width(),
-                        definition.size().height()
-                )
-        );
+        tooltip.add(TooltipHelper.category(definition.category()));
+        tooltip.add(TooltipHelper.size(definition.size().width(), definition.size().height()));
 
         if (definition.energy().isPresent()) {
-            tooltip.add(
-                    TooltipHelper.energyConsumption(
-                            definition.energy().get().consumption()
-                    )
-            );
+            tooltip.add(TooltipHelper.energyConsumption(definition.energy().get().consumption()));
         }
 
         if (definition.generation().isPresent()) {
-            tooltip.add(
-                    TooltipHelper.energyGeneration(
-                            definition.generation().get().generation()
-                    )
-            );
+            tooltip.add(TooltipHelper.energyGeneration(definition.generation().get().generation()));
         }
 
         if (definition.storage().isPresent()) {
-            tooltip.add(
-                    TooltipHelper.input(
-                            definition.storage().get().maxInput()
-                    )
+            var storage = definition.storage().get();
+            tooltip.add(TooltipHelper.input(storage.maxInput()));
+            tooltip.add(TooltipHelper.output(storage.maxOutput()));
+            int stored = Math.min(
+                    stack.getOrDefault(ModDataComponents.MODULE_STORED_ENERGY.get(), 0),
+                    storage.capacity()
             );
-
-            tooltip.add(
-                    TooltipHelper.output(
-                            definition.storage().get().maxOutput()
-                    )
-            );
-
-            //ToDo дополнить описание текущим зарядом
-            tooltip.add(
-                    TooltipHelper.capacity(
-                            definition.storage().get().capacity()
-                    )
-            );
+            tooltip.add(TooltipHelper.capacity(stored, storage.capacity()));
         }
 
         if (definition.thermal().isPresent()) {
-            tooltip.add(
-                    TooltipHelper.cooling(
-                            definition.thermal().get().cooling()
-                    )
-            );
-
-            tooltip.add(
-                    TooltipHelper.heatGeneration(
-                            definition.thermal().get().heatGeneration()
-                    )
-            );
+            tooltip.add(TooltipHelper.cooling(definition.thermal().get().cooling()));
+            tooltip.add(TooltipHelper.heatGeneration(definition.thermal().get().heatGeneration()));
         }
     }
 }

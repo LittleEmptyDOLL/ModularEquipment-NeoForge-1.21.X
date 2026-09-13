2da76c3a8f4b29f6694a71046f8cfff3bb567ad4
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-14 00:38:57 +0300

    Use NeoForge external energy capability in exoskeleton tick

--- src/main/java/com/github/littleemptydoll/exoequipment/item/ExoskeletonItem.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/item/ExoskeletonItem.java
@@ -3,6 +3,7 @@ package com.github.littleemptydoll.exoequipment.item;
 import com.github.littleemptydoll.exoequipment.client.TooltipHelper;
 import com.github.littleemptydoll.exoequipment.energy.EnergyOperations;
 import com.github.littleemptydoll.exoequipment.energy.EnergyTickResult;
+import com.github.littleemptydoll.exoequipment.energy.NeoForgeEnergyProvider;
 import com.github.littleemptydoll.exoequipment.exoskeleton.*;
 import com.github.littleemptydoll.exoequipment.registry.EquipmentItem;
 import com.github.littleemptydoll.exoequipment.registry.ModDataComponents;
@@ -74,7 +75,13 @@ public class ExoskeletonItem extends EquipmentItem<ExoskeletonDefinition> implem
             return;
         }
 
-        EnergyTickResult result = EnergyOperations.tick(getData(stack));
+        NeoForgeEnergyProvider externalEnergy =
+                NeoForgeEnergyProvider.fromEntity(slotContext.entity());
+
+        EnergyTickResult result = EnergyOperations.tick(
+                getData(stack),
+                externalEnergy
+        );
 
         if (!result.data().equals(getData(stack))) {
             stack.set(

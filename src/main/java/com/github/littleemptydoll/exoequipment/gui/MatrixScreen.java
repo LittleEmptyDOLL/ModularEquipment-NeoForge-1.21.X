42ca71ff97a72a6d9bd5a4611caaea6537cf9aeb
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-13 03:43:34 +0300

    Clean MatrixScreen imports

--- src/main/java/com/github/littleemptydoll/exoequipment/gui/MatrixScreen.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/gui/MatrixScreen.java
@@ -10,12 +10,9 @@ import com.github.littleemptydoll.exoequipment.network.MatrixActionPayload;
 import com.github.littleemptydoll.exoequipment.registry.ModModules;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ServerboundContainerClickPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.network.PacketDistributor;
 

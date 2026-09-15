6de13197bd698cfa357fda1cb0323df3ab4a6a3c
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-15 23:31:05 +0300

    Use revised profile GUI screen

--- src/main/java/com/github/littleemptydoll/exoequipment/event/ClientModEvents.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/event/ClientModEvents.java
@@ -1,7 +1,7 @@
 package com.github.littleemptydoll.exoequipment.event;
 
 import com.github.littleemptydoll.exoequipment.ExoEquipment;
-import com.github.littleemptydoll.exoequipment.gui.ExoskeletonProfileScreen;
+import com.github.littleemptydoll.exoequipment.gui.ExoskeletonProfilesScreen;
 import com.github.littleemptydoll.exoequipment.gui.ExoskeletonScreen;
 import com.github.littleemptydoll.exoequipment.gui.MatrixScreen;
 import com.github.littleemptydoll.exoequipment.registry.ModMenus;
@@ -34,7 +34,7 @@ public final class ClientModEvents {
 
         event.register(
                 ModMenus.EXOSKELETON_PROFILES.get(),
-                ExoskeletonProfileScreen::new
+                ExoskeletonProfilesScreen::new
         );
     }
 }

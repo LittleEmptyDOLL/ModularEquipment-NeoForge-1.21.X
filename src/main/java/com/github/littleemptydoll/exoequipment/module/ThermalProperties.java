7587b0cedd9e2ae4e8a169ab893a6cded0576688
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-18 23:49:27 +0300

    Use double precision for thermal properties

--- src/main/java/com/github/littleemptydoll/exoequipment/module/ThermalProperties.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/module/ThermalProperties.java
@@ -4,16 +4,16 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 public record ThermalProperties(
-        int heatGeneration,
-        int cooling
+        double heatGeneration,
+        double cooling
 ) {
     public static final Codec<ThermalProperties> CODEC =
             RecordCodecBuilder.create(instance ->
                     instance.group(
-                            Codec.INT
+                            Codec.DOUBLE
                                     .fieldOf("heat_generation")
                                     .forGetter(ThermalProperties::heatGeneration),
-                            Codec.INT
+                            Codec.DOUBLE
                                     .fieldOf("cooling")
                                     .forGetter(ThermalProperties::cooling)
                     ).apply(
@@ -23,13 +23,13 @@ public record ThermalProperties(
             );
 
     public ThermalProperties {
-        if (heatGeneration < 0) {
+        if (heatGeneration < 0.0D) {
             throw new IllegalArgumentException(
                     "Heat generation cannot be negative"
             );
         }
 
-        if (cooling < 0) {
+        if (cooling < 0.0D) {
             throw new IllegalArgumentException(
                     "Cooling cannot be negative"
             );

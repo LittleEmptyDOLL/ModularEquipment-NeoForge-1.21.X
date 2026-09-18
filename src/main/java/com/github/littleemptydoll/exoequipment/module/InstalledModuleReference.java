71d9eca38355a19dcd2eef010ced541b8f23c0b0
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-19 02:12:08 +0300

    Уточнить состояние и порядок обработки щитов

--- src/main/java/com/github/littleemptydoll/exoequipment/module/InstalledModuleReference.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/module/InstalledModuleReference.java
@@ -1,9 +1,27 @@
 package com.github.littleemptydoll.exoequipment.module;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 public record InstalledModuleReference(
         int matrixSlot,
         int moduleIndex
 ) {
+    public static final Codec<InstalledModuleReference> CODEC =
+            RecordCodecBuilder.create(instance ->
+                    instance.group(
+                            Codec.INT
+                                    .fieldOf("matrix_slot")
+                                    .forGetter(InstalledModuleReference::matrixSlot),
+                            Codec.INT
+                                    .fieldOf("module_index")
+                                    .forGetter(InstalledModuleReference::moduleIndex)
+                    ).apply(
+                            instance,
+                            InstalledModuleReference::new
+                    )
+            );
+
     public InstalledModuleReference {
         if (matrixSlot < 0) {
             throw new IllegalArgumentException("Matrix slot cannot be negative");

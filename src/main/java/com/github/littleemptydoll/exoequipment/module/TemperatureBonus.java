1db2bf4fd36db64a38d63f4bd3ea368f3e1e5357
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-18 11:24:34 +0300

    Add temperature bonus definition

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/module/TemperatureBonus.java
@@ -0,0 +1,42 @@
+package com.github.littleemptydoll.exoequipment.module;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public record TemperatureBonus(
+        double minTemperature,
+        double maxTemperature,
+        double maximumBonus
+) {
+    public static final Codec<TemperatureBonus> CODEC =
+            RecordCodecBuilder.create(instance ->
+                    instance.group(
+                            Codec.DOUBLE
+                                    .fieldOf("min_temperature")
+                                    .forGetter(TemperatureBonus::minTemperature),
+                            Codec.DOUBLE
+                                    .fieldOf("max_temperature")
+                                    .forGetter(TemperatureBonus::maxTemperature),
+                            Codec.DOUBLE
+                                    .fieldOf("maximum_bonus")
+                                    .forGetter(TemperatureBonus::maximumBonus)
+                    ).apply(
+                            instance,
+                            TemperatureBonus::new
+                    )
+            );
+
+    public TemperatureBonus {
+        if (minTemperature > maxTemperature) {
+            throw new IllegalArgumentException(
+                    "Temperature bonus minimum cannot exceed maximum"
+            );
+        }
+
+        if (maximumBonus < 0) {
+            throw new IllegalArgumentException(
+                    "Maximum temperature bonus cannot be negative"
+            );
+        }
+    }
+}

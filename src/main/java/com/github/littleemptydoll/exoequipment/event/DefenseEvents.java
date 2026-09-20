1eb8989533cd4d7140ae014e0d8d97100489681b
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-20 18:51:01 +0300

    Remove legacy damage chance protection

--- src/main/java/com/github/littleemptydoll/exoequipment/event/DefenseEvents.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/event/DefenseEvents.java
@@ -61,14 +61,6 @@ public final class DefenseEvents {
 
         DamageSource source = event.getSource();
 
-        if (com.github.littleemptydoll.exoequipment.module.DamageChanceProtectionOperations.blocksDamage(
-                data,
-                runtime.poweredModules()
-        )) {
-            event.setCanceled(true);
-            return;
-        }
-
         ResourceLocation damageType =
                 source.typeHolder()
                         .unwrapKey()

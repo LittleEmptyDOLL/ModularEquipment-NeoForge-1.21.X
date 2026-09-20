cce42caacfa9367da475b37c8cd0d640d5ceed05
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-20 17:18:06 +0300

    Register optional Apothic combat integration

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/compat/apothicattributes/ApothicAttributesIntegration.java
@@ -0,0 +1,13 @@
+package com.github.littleemptydoll.exoequipment.compat.apothicattributes;
+
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.NeoForge;
+
+public final class ApothicAttributesIntegration {
+    private ApothicAttributesIntegration() {}
+
+    public static void register(IEventBus modEventBus) {
+        NeoForge.EVENT_BUS.register(ApothicCombatEvents.class);
+    }
+}

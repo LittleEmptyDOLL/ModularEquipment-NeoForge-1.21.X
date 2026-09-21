836babdc4f8ad73e1dd86441a1d1045b6a59fd61
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-21 08:14:09 +0300

    Split module codec properties to avoid DFU group limit

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/module/ModuleExtendedProperties.java
@@ -0,0 +1,54 @@
+package com.github.littleemptydoll.exoequipment.module;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import java.util.Optional;
+
+public record ModuleExtendedProperties(
+        Optional<BlinkProperties> blink,
+        Optional<FlightProperties> flight,
+        Optional<AttributeProperties> attributes,
+        Optional<ConditionalAttributeProperties> conditionalAttributes,
+        Optional<PickupMagnetProperties> pickupMagnet,
+        Optional<TemperatureModifierProperties> temperatureModifier,
+        Optional<TemperatureImpactProperties> temperatureImpact,
+        Optional<BodyDamageProtectionProperties> bodyDamageProtection,
+        Optional<BodyDamageRegenerationProperties> bodyDamageRegeneration,
+        Optional<ThirstProperties> thirst,
+        Optional<PainkillerProperties> painkiller,
+        Optional<BlockScannerProperties> blockScanner,
+        Optional<FallProtectionProperties> fallProtection
+) {
+    public static final MapCodec<ModuleExtendedProperties> CODEC =
+            RecordCodecBuilder.mapCodec(instance ->
+                    instance.group(
+                            BlinkProperties.CODEC.optionalFieldOf("blink")
+                                    .forGetter(ModuleExtendedProperties::blink),
+                            FlightProperties.CODEC.optionalFieldOf("flight")
+                                    .forGetter(ModuleExtendedProperties::flight),
+                            AttributeProperties.CODEC.optionalFieldOf("attributes")
+                                    .forGetter(ModuleExtendedProperties::attributes),
+                            ConditionalAttributeProperties.CODEC.optionalFieldOf("conditional_attributes")
+                                    .forGetter(ModuleExtendedProperties::conditionalAttributes),
+                            PickupMagnetProperties.CODEC.optionalFieldOf("pickup_magnet")
+                                    .forGetter(ModuleExtendedProperties::pickupMagnet),
+                            TemperatureModifierProperties.CODEC.optionalFieldOf("temperature_modifier")
+                                    .forGetter(ModuleExtendedProperties::temperatureModifier),
+                            TemperatureImpactProperties.CODEC.optionalFieldOf("temperature_impact")
+                                    .forGetter(ModuleExtendedProperties::temperatureImpact),
+                            BodyDamageProtectionProperties.CODEC.optionalFieldOf("body_damage_protection")
+                                    .forGetter(ModuleExtendedProperties::bodyDamageProtection),
+                            BodyDamageRegenerationProperties.CODEC.optionalFieldOf("body_damage_regeneration")
+                                    .forGetter(ModuleExtendedProperties::bodyDamageRegeneration),
+                            ThirstProperties.CODEC.optionalFieldOf("thirst")
+                                    .forGetter(ModuleExtendedProperties::thirst),
+                            PainkillerProperties.CODEC.optionalFieldOf("painkiller")
+                                    .forGetter(ModuleExtendedProperties::painkiller),
+                            BlockScannerProperties.CODEC.optionalFieldOf("block_scanner")
+                                    .forGetter(ModuleExtendedProperties::blockScanner),
+                            FallProtectionProperties.CODEC.optionalFieldOf("fall_protection")
+                                    .forGetter(ModuleExtendedProperties::fallProtection)
+                    ).apply(instance, ModuleExtendedProperties::new)
+            );
+}

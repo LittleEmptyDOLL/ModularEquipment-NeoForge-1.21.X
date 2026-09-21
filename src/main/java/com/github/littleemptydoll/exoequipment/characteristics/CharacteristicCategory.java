b87fc826aa6ac9a5cc54b48a201a6082b8b289f7
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-22 00:16:39 +0300

    Add initial characteristics model and provider

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/characteristics/CharacteristicCategory.java
@@ -0,0 +1,14 @@
+package com.github.littleemptydoll.exoequipment.characteristics;
+
+public enum CharacteristicCategory {
+    ENERGY,
+    THERMAL,
+    DEFENSE,
+    SHIELD,
+    ATTRIBUTES,
+    REGENERATION,
+    MOBILITY,
+    SURVIVAL,
+    SENSOR,
+    UTILITY
+}

dab82bf8d3c4bc95501eb498fb9d0b88573b2aab
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-24 12:31:57 +0300

    Remove dedicated defense/effect mechanics

--- src/main/java/com/github/littleemptydoll/exoequipment/module/NightVisionProperties.java
+++
@@ -1,8 +0,0 @@
-package com.github.littleemptydoll.exoequipment.module;
-
-import com.mojang.serialization.Codec;
-
-public record NightVisionProperties() {
-    public static final Codec<NightVisionProperties> CODEC =
-            Codec.unit(new NightVisionProperties());
-}

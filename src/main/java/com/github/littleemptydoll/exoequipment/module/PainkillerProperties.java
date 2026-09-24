dcffbebf033ad6c4db4b73010f1632949f58b3f5
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-24 12:32:01 +0300

    Remove dedicated defense/effect mechanics

--- src/main/java/com/github/littleemptydoll/exoequipment/module/PainkillerProperties.java
+++
@@ -1,8 +0,0 @@
-package com.github.littleemptydoll.exoequipment.module;
-
-import com.mojang.serialization.Codec;
-
-public record PainkillerProperties() {
-    public static final Codec<PainkillerProperties> CODEC =
-            Codec.unit(new PainkillerProperties());
-}

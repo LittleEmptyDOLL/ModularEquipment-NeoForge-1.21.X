42124865d62cd7fcd3e457ee9522b12dada4e533
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-26 01:20:43 +0300

    Add shared read-only slot

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/gui/ReadOnlySlot.java
@@ -0,0 +1,27 @@
+package com.github.littleemptydoll.exoequipment.gui;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
+public final class ReadOnlySlot extends Slot {
+    public ReadOnlySlot(
+            Container container,
+            int slot,
+            int x,
+            int y
+    ) {
+        super(container, slot, x, y);
+    }
+
+    @Override
+    public boolean mayPlace(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public boolean mayPickup(Player player) {
+        return false;
+    }
+}

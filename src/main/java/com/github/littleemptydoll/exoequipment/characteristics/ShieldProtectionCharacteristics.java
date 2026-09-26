cfd9f7754b12be9915198efbca862c91a2704290
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-26 19:17:27 +0300

    Show shield protection transfer characteristic

---
+++ src/main/java/com/github/littleemptydoll/exoequipment/characteristics/ShieldProtectionCharacteristics.java
@@ -0,0 +1,38 @@
+package com.github.littleemptydoll.exoequipment.characteristics;
+
+import com.github.littleemptydoll.exoequipment.module.ShieldProtectionOperations;
+
+import java.util.List;
+
+final class ShieldProtectionCharacteristics {
+    private ShieldProtectionCharacteristics() {}
+
+    static void add(
+            List<Characteristic> result,
+            CharacteristicsContext context
+    ) {
+        double transfer = context.isMatrixScope()
+                ? ShieldProtectionOperations.calculateTransfer(
+                        context.data(),
+                        context.matrixSlot(),
+                        CharacteristicsSupport.poweredModules(context)
+                )
+                : ShieldProtectionOperations.calculateTransfer(
+                        context.data(),
+                        CharacteristicsSupport.poweredModules(context)
+                );
+
+        if (transfer <= 0.0D) {
+            return;
+        }
+
+        result.add(
+                new Characteristic(
+                        CharacteristicCategory.SHIELD,
+                        "protection_transfer",
+                        CharacteristicType.CURRENT,
+                        transfer
+                )
+        );
+    }
+}

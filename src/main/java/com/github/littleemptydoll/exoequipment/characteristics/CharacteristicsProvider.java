2e768cad17bbff2c8dbd43b5b22e5087356c8543
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-26 13:14:45 +0300

    Include effects and status protection in characteristics

--- src/main/java/com/github/littleemptydoll/exoequipment/characteristics/CharacteristicsProvider.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/characteristics/CharacteristicsProvider.java
@@ -16,8 +16,10 @@ public final class CharacteristicsProvider {
         EnergyCharacteristics.add(result, context);
         ThermalCharacteristics.add(result, context);
         DefenseCharacteristics.add(result, context);
+        StatusProtectionCharacteristics.add(result, context);
         SurvivalCharacteristics.add(result, context);
         AttributeCharacteristics.add(result, context);
+        EffectsCharacteristics.add(result, context);
         MobilityCharacteristics.add(result, context);
         SensorCharacteristics.add(result, context);
         UtilityCharacteristics.add(result, context);

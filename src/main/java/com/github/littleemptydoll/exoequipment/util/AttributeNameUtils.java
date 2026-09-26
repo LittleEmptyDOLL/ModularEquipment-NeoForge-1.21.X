8c8c55599a992b5c63900c9bc60f1bcbe8dc3b4e
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-26 13:13:03 +0300

    Use registered attribute display names

--- src/main/java/com/github/littleemptydoll/exoequipment/util/AttributeNameUtils.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/util/AttributeNameUtils.java
@@ -1,5 +1,6 @@
 package com.github.littleemptydoll.exoequipment.util;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -7,13 +8,31 @@ public final class AttributeNameUtils {
     private AttributeNameUtils() {}
 
     public static Component getName(ResourceLocation id) {
-        String name = id.toString();
-        int separator = Math.max(name.lastIndexOf('.'), name.lastIndexOf(':'));
+        if (id != null) {
+            var holder = BuiltInRegistries.ATTRIBUTE.getHolder(id).orElse(null);
+
+            if (holder != null) {
+                return Component.translatable(
+                        holder.value().getDescriptionId()
+                );
+            }
+        }
+
+        return Component.literal(fallbackName(id));
+    }
+
+    private static String fallbackName(ResourceLocation id) {
+        if (id == null) {
+            return "Unknown Attribute";
+        }
+
+        String name = id.getPath();
+        int separator = name.lastIndexOf('.');
 
         if (separator >= 0) {
             name = name.substring(separator + 1);
         }
 
-        return Component.literal(NameUtils.toDisplayName(name));
+        return NameUtils.toDisplayName(name);
     }
 }

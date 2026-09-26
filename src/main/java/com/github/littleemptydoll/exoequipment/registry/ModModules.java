85b1cea51d65e78e7730a23c0f63f0b430311aaa
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-26 16:58:25 +0300

    Register production defense modules

--- src/main/java/com/github/littleemptydoll/exoequipment/registry/ModModules.java
+++ src/main/java/com/github/littleemptydoll/exoequipment/registry/ModModules.java
@@ -33,6 +33,7 @@ public final class ModModules {
 
     static {
         ModEnergyModules.register(REGISTRY);
+        ModDefenseModules.register(REGISTRY);
     }
 
     static void registerTransientDefinition(ModuleDefinition definition) {
